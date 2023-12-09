f078d66f0c2a8402271203ab8498920e16cb5228
shyunc <scho12@risd.edu>
2023-12-08 21:42:00 -0500

    more organizing!! lol

--- projects/p3-input-output/common/script.js
+++ projects/p3-input-output/common/script.js
@@ -1,50 +1,19 @@
+// div1.addEventListener("animationstart", myStartFunction);
+// div1.addEventListener("animationend", myEndFunction);
 
-// function initMap() {
-//     map = new google.maps.Map(document.getElementById('map'), {
-//         center: { lat: 41.763031, lng: -73.044465 },
-//         zoom: 17.75
-//     });
-
-//     google.maps.event.addListener(map, 'click', function (event) {
-//         new google.maps.Marker({
-//             position: event.latLng,
-//             map: map,
-//         });
-//     });
+// function myStartFunction() {
+//   this.innerHTML = "The animation has started";
+// 
 // }
 
-// const btn = document.getElementsByClassName('centered');
+// }
+// function myEndFunction() {
+//   this.innerHTML = "The animation has completed";
+//   this.style.backgroundColor = "lightgray";
+// }
 
-// btn.addEventListener('click', () => {
-//   // 👇️ hide button
-//   btn.style.display = 'none';
 
-//   // 👇️ show div
-//   const box = document.getElementsByClassName('centered');
-//   box.style.display = 'block';
-// });
-
-// const btn = document.getElementsByClassName('centered');
-
-// btn.addEventListener('click', () => {
-//   setTimeout(() => {
-//     const box = document.getElementsByClassName('enter');
-
-//     // 👇️ removes element from DOM
-//     // box.style.display = 'none';
-
-//     // 👇️ hides element (still takes up space on page)
-//     box.style.visibility = 'hidden';
-//   }, 1000);
-// });
-
-var xV1 = 0;
-function stayHome(){
-nV = document.body.scrollTop;
-document.getElementByClassName('centered').style.top = xV1+nV;
-}
-function init(){
-xV1 = document.getElementByClassName('centered').offsetTop;
-}
-window.onload=init;
-window.onscroll=stayHome;
+// not working
+$("#button").click(function() {
+    $('.transform').toggleClass('.transform-active');
+  });
--- projects/p3-input-output/hub/script.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("redirectButton").addEventListener("click", redirect);
-
-function redirect(){ window.location = "https://www.google.com"; }
-
